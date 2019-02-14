788cf684109fbad0c683c7fa172094604ba76b58
SherWul <sherli.fx@gmail.com>
2019-02-14 09:16:38 +0700

    Create note pembelian.cpp
    untuk note Digital Pembelian atau Penjualan di Dev C++

---
+++ note pembelian.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+#include<string>
+using namespace std;
+int main () {
+	string no,tg,tanggal,ns,nm,nama,name,kl,ps,jl,JL,telepon;
+	
+				cout<<"			IT Service				"<<endl;
+				cout<<"		ACP SMKN 4 PADALARANG		"<<endl;
+				cout<<"		Jl.Raya Padalarang 451		"<<endl;				
+	cout<<"--------------------------------------------------------------"<<endl;
+	
+
+	
+	
+	cout<<"No.:";cin>>no;					cout<<"Jenis Laptop:";cin>>jl>>JL;  //gabisa di kepinggirin gatau caranya
+	cout<<"Tanggal:";cin>>tg>>tanggal;		cout<<"No.Seri:";cin>>ns;
+	cout<<"Nama:";cin>>nm>>nama>>name;		cout<<"Telepon:";cin>>telepon;
+	cout<<endl;
+	cout<<"Form Keluhan"<<endl;
+			cout<<"Keluhan Laptop:";
+			cin>>kl;
+			cout<<"Perkiraan Selesai:";
+			cin>>ps;
+	
+	system("PAUSE");
+	return 0;
+}
